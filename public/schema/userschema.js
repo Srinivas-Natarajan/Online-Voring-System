6480bc72c8f6759b943cd20760246986d0f5e194
Srinivas Natarajan <nsrinivas06@gmail.com>
2021-03-15 18:02:03 +0530

    Commited

---
+++ public/schema/userschema.js
@@ -0,0 +1,14 @@
+var mongoose=require("mongoose"),
+passportLocalmongoose=require("passport-local-mongoose");
+var UserSchema=new mongoose.Schema({
+    username: {type:String, unique:true},
+    password:String,
+	email:String,
+	uniqueCode:{type:String, unique:true},
+	phoneno:Number,
+	candidate:String,
+	adhaar:{type:Number,unique:true},
+	created:{type: Date,default: Date.now}
+});
+UserSchema.plugin(passportLocalmongoose);
+module.exports = mongoose.model("User",UserSchema);
